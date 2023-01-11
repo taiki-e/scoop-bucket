c11e3d37522bfcab8003de457a4c8004a04cd70a
Taiki Endo <te316e89@gmail.com>
2023-01-11 21:53:39 +0900

    Add cargo-no-dev-deps

--- tools/bucket.sh
+++ tools/bucket.sh
@@ -16,12 +16,14 @@ packages=(
     "cargo-hack"             # https://github.com/taiki-e/cargo-hack
     "cargo-llvm-cov"         # https://github.com/taiki-e/cargo-llvm-cov
     "cargo-minimal-versions" # https://github.com/taiki-e/cargo-minimal-versions
+    "cargo-no-dev-deps"      # https://github.com/taiki-e/cargo-no-dev-deps
     "parse-changelog"        # https://github.com/taiki-e/parse-changelog
 )
 descriptions=(
     "Cargo subcommand for testing and continuous integration"
     "Cargo subcommand for LLVM source-based code coverage (-C instrument-coverage)"
     "Cargo subcommand for proper use of -Z minimal-versions"
+    "Cargo subcommand for running cargo without dev-dependencies"
     "Simple changelog parser, written in Rust"
 )
 
