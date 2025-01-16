9c986aac186f04873b91f5acf786af4709ca6838
Taiki Endo <te316e89@gmail.com>
2025-01-16 22:33:35 +0900

    tools: Format shell scripts with indent_size = 2 to match scripts in CI config

--- tools/bucket.sh
+++ tools/bucket.sh
@@ -12,69 +12,69 @@ cd -- "$(dirname -- "$0")"/..
 
 owner="taiki-e"
 packages=(
-    "cargo-hack"             # https://github.com/taiki-e/cargo-hack
-    "cargo-llvm-cov"         # https://github.com/taiki-e/cargo-llvm-cov
-    "cargo-minimal-versions" # https://github.com/taiki-e/cargo-minimal-versions
-    "cargo-no-dev-deps"      # https://github.com/taiki-e/cargo-no-dev-deps
-    "parse-changelog"        # https://github.com/taiki-e/parse-changelog
+  "cargo-hack"             # https://github.com/taiki-e/cargo-hack
+  "cargo-llvm-cov"         # https://github.com/taiki-e/cargo-llvm-cov
+  "cargo-minimal-versions" # https://github.com/taiki-e/cargo-minimal-versions
+  "cargo-no-dev-deps"      # https://github.com/taiki-e/cargo-no-dev-deps
+  "parse-changelog"        # https://github.com/taiki-e/parse-changelog
 )
 descriptions=(
-    "Cargo subcommand for testing and continuous integration"
-    "Cargo subcommand for LLVM source-based code coverage (-C instrument-coverage)"
-    "Cargo subcommand for proper use of -Z minimal-versions"
-    "Cargo subcommand for running cargo without dev-dependencies"
-    "Simple changelog parser, written in Rust"
+  "Cargo subcommand for testing and continuous integration"
+  "Cargo subcommand for LLVM source-based code coverage (-C instrument-coverage)"
+  "Cargo subcommand for proper use of -Z minimal-versions"
+  "Cargo subcommand for running cargo without dev-dependencies"
+  "Simple changelog parser, written in Rust"
 )
 
 retry() {
-    for i in {1..10}; do
-        if "$@"; then
-            return 0
-        else
-            sleep "${i}"
-        fi
-    done
-    "$@"
+  for i in {1..10}; do
+    if "$@"; then
+      return 0
+    else
+      sleep "${i}"
+    fi
+  done
+  "$@"
 }
 info() {
-    printf >&2 'info: %s\n' "$*"
+  printf >&2 'info: %s\n' "$*"
 }
 run_curl() {
-    if [[ -n "${GITHUB_TOKEN:-}" ]]; then
-        retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 --retry-connrefused \
-            -H "Authorization: token ${GITHUB_TOKEN}" \
-            "$@"
-    else
-        retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 --retry-connrefused \
-            "$@"
-    fi
+  if [[ -n "${GITHUB_TOKEN:-}" ]]; then
+    retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 --retry-connrefused \
+      -H "Authorization: token ${GITHUB_TOKEN}" \
+      "$@"
+  else
+    retry curl --proto '=https' --tlsv1.2 -fsSL --retry 10 --retry-connrefused \
+      "$@"
+  fi
 }
 
 for i in "${!packages[@]}"; do
-    package="${packages[${i}]}"
-    info "fetching latest version of ${package}"
-    tag=$(run_curl "https://api.github.com/repos/${owner}/${package}/releases/latest" | jq -r '.tag_name')
-    x86_64_url="https://github.com/${owner}/${package}/releases/download/${tag}/${package}-x86_64-pc-windows-msvc.zip"
-    info "downloading ${x86_64_url} for checksum"
-    x86_64_sha=$(run_curl "${x86_64_url}" | sha256sum)
-    aarch64=''
-    case "${package}" in
-        cargo-llvm-cov) ;; # TODO
-        *)
-            aarch64_url="https://github.com/${owner}/${package}/releases/download/${tag}/${package}-aarch64-pc-windows-msvc.zip"
-            info "downloading ${aarch64_url} for checksum"
-            aarch64_sha=$(run_curl "${aarch64_url}" | sha256sum)
-            aarch64=",
+  package="${packages[${i}]}"
+  info "fetching latest version of ${package}"
+  tag=$(run_curl "https://api.github.com/repos/${owner}/${package}/releases/latest" | jq -r '.tag_name')
+  x86_64_url="https://github.com/${owner}/${package}/releases/download/${tag}/${package}-x86_64-pc-windows-msvc.zip"
+  info "downloading ${x86_64_url} for checksum"
+  x86_64_sha=$(run_curl "${x86_64_url}" | sha256sum)
+  aarch64=''
+  case "${package}" in
+    cargo-llvm-cov) ;; # TODO
+    *)
+      aarch64_url="https://github.com/${owner}/${package}/releases/download/${tag}/${package}-aarch64-pc-windows-msvc.zip"
+      info "downloading ${aarch64_url} for checksum"
+      aarch64_sha=$(run_curl "${aarch64_url}" | sha256sum)
+      aarch64=",
     \"arm64\": {
       \"url\": \"${aarch64_url}\",
       \"hash\": \"${aarch64_sha%% *}\"
     }"
-            ;;
-    esac
+      ;;
+  esac
 
-    # Refs: https://scoop-docs.vercel.app/docs/concepts/App-Manifests.html
-    # suggest:vcredist is not needed because their windows binaries are static executables.
-    cat >|./bucket/"${package}".json <<EOF
+  # Refs: https://scoop-docs.vercel.app/docs/concepts/App-Manifests.html
+  # suggest:vcredist is not needed because their windows binaries are static executables.
+  cat >|./bucket/"${package}".json <<EOF
 {
   "version": "${tag#v}",
   "description": "${descriptions[${i}]}",
